ed5d7646963f0dda8509940bfbde7847bf9ccfdc
Bhavana Dalvi <bhavanad@allenai.org>
2018-03-07 15:54:22 -0800

    Adding ProLocal model
    - Dataset reader
    - Model
    - Predictor
    - Test-cases for these 3

---
+++ propara/run.py
@@ -0,0 +1,19 @@
+import logging
+import os
+import sys
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir))))
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
+                    level=logging.INFO)
+
+from allennlp.commands import main
+
+from propara.data.prolocal_dataset_reader import ProLocalDatasetReader
+from propara.models.prolocal_model import ProLocalModel
+from propara.service.predictors.prolocal_prediction import ProLocalPredictor
+
+if __name__ == "__main__":
+    predictor_overrides = {
+                           'ProLocalModel': 'prolocal-prediction',
+                           }
+    main(prog="python -m allennlp.run", predictor_overrides=predictor_overrides)
---
+++ propara/service/predictors/prolocal_prediction.py
@@ -0,0 +1,57 @@
+from overrides import overrides
+from typing import List
+
+from allennlp.common.util import JsonDict, sanitize
+from allennlp.data import Instance
+from allennlp.service.predictors.predictor import Predictor
+from propara.models.prolocal_model import ProLocalModel
+from propara.data.prolocal_dataset_reader import ProLocalDatasetReader
+
+@Predictor.register('prolocal-prediction')
+class ProLocalPredictor(Predictor):
+    """
+    Wrapper for the :class:`processes.models.StateChangeModelCombinedLossBothAttention` model.
+    """
+    @overrides
+    def predict_json(self, inputs: JsonDict, cuda_device: int = -1) -> JsonDict:
+       instance_text = inputs["instance"]
+       parts: List[str] = instance_text.split("++++")
+
+       print(len(parts))
+       print(instance_text)
+
+       paraid = parts[1]
+       sentenceid = parts[2]
+       sentence = parts[3]
+       entity = parts[4]
+
+       sentence_tokens = parts[5].split("####")
+       verb_span = parts[6].split(",")
+       verb_vector = [int(i) for i in verb_span]
+       entity_span = parts[7].split(",")
+       entity_vector = [int(i) for i in entity_span]
+
+       state_change_types = parts[8]
+       state_change_tags = parts[9].split(",")
+
+       instance = self._dataset_reader.text_to_instance(sentence_tokens=sentence_tokens,
+                                                        verb_vector=verb_vector,
+                                                        entity_vector=entity_vector,
+                                                        state_change_types=state_change_types,
+                                                        state_change_tags=state_change_tags)
+       outputs = self._model.forward_on_instance(instance, cuda_device)
+
+       predictions = {}
+       predictions["paraid"] = paraid
+       predictions["sentenceid"] = sentenceid
+       predictions["sentence"] = sentence
+       predictions["sentence_tokens"] = sentence_tokens
+       predictions["entity"] = entity
+
+       predictions["predicted_types"] = outputs["predicted_types"]
+       predictions["predicted_tags"] = outputs["predicted_tags"]
+
+       predictions["gold_types"] = state_change_types
+       predictions["gold_tags"] = state_change_tags
+
+       return predictions
