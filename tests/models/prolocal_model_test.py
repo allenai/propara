c99b0023431322e158d49f18ff043a660dcf2fcc
Bhavana Dalvi <bhavanad@allenai.org>
2018-03-07 15:57:21 -0800

    Added test cases for ProLocal
    Added tests for
    - dataset reader
    - model
    - predictor

---
+++ tests/models/prolocal_model_test.py
@@ -0,0 +1,18 @@
+# pylint: disable=no-self-use,invalid-name
+
+from allennlp.common.testing import ModelTestCase
+from flaky import flaky
+from propara.data.prolocal_dataset_reader import ProLocalDatasetReader
+from propara.models.prolocal_model import ProLocalModel
+
+class StateChangeAttentionModelTest(ModelTestCase):
+    def setUp(self):
+        super(StateChangeAttentionModelTest, self).setUp()
+        self.set_up_model('tests/fixtures/prolocal_params.json', 'tests/fixtures/prolocal_toy_data.tsv')
+
+    def test_state_change_model_can_train_save_and_load(self):
+        self.ensure_model_can_train_save_and_load(self.param_file)
+
+    @flaky
+    def ensure_batch_predictions_are_consistent(self):
+        self.ensure_batch_predictions_are_consistent()
