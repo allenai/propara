e6eaf61cabbf79fcd665b08fdcfcdbc9572b453a
Bhavana Dalvi <bhavana.dalvi@gmail.com>
2018-05-30 09:35:00 -0700

    Adding prolocal data and updating eval script (#8)
    * Adding eval script and associated data files
    
    * minor change in combined_metric
    
    * adding naacl data and output for prolocal and minor changes to run.py
    
    * minor changes in run.py

--- propara/run.py
+++ propara/run.py
@@ -12,5 +12,6 @@ from propara.data.prolocal_dataset_reader import ProLocalDatasetReader
 from propara.models.prolocal_model import ProLocalModel
 from propara.service.predictors.prolocal_prediction import ProLocalPredictor
 
+
 if __name__ == "__main__":
     main(prog="python -m allennlp.run")
